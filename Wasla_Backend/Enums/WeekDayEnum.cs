67b6a7d490cbd4d0d96c3309f4af98b16a5d819d
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-12-05 19:15:47 +0200

    Update WeekDayEnum

--- Wasla_Backend/Enums/WeekDayEnum.cs
+++ Wasla_Backend/Enums/WeekDayEnum.cs
@@ -2,13 +2,13 @@
 {
     public enum WeekDayEnum
     {
-        None = 0,
-        Saturday = 1,
-        Sunday,
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday
+        none = 0,
+        saturday,
+        sunday,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday
     }
 }
