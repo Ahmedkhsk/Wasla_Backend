6632631c13359a2c25b2b01aec967a548d0825f1
kareembadr28 <bdrkrym558@gmail.com>
2025-12-23 01:28:19 +0200

    searchresponsedto

---
+++ Wasla_Backend/DTOs/SearchResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Wasla_Backend.DTOs
+{
+    public class SearchResponse
+    {
+        public int id { get; set; }
+        public ServiceProviderType type { get; set; }
+        public dynamic details { get; set; }
+    }
+}
