6e75d3065cec38567075ec3ce31015b717a7359c
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2026-03-18 22:58:32 +0200

    Refactor Chat Model

---
+++ Wasla_Backend/Data/Migrations/20260318202414_updateChatModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wasla_Backend.data
+{
+    /// <inheritdoc />
+    public partial class updateChatModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "deletedByReceiverId",
+                table: "Chats",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "deletedBySenderId",
+                table: "Chats",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "receiverDeletedAt",
+                table: "Chats",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "senderDeletedAt",
+                table: "Chats",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deletedByReceiverId",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "deletedBySenderId",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "receiverDeletedAt",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "senderDeletedAt",
+                table: "Chats");
+        }
+    }
+}
