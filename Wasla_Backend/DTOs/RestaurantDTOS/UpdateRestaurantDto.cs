e3985060ceeec68285853285bc1f7a2a4553c9dc
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2026-04-13 04:00:55 +0200

    WR-46 Backend

---
+++ Wasla_Backend/DTOs/RestaurantDTOS/UpdateRestaurantDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Wasla_Backend.DTOs.RestaurantDTOS
+{
+    public class UpdateRestaurantDto
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string phoneNumber { get; set; }
+        public string ownerName { get; set; }
+        public int restaurantCategoryId { get; set; }
+        public int numberOfTables { get; set; }
+        public int numberOfPersons { get; set; }
+        public IFormFile? profile { get; set; }
+        public List<IFormFile>? gallery { get; set; }
+    }
+}
